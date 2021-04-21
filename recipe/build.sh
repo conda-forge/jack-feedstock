6ace9d9c0971723dd868598066246741b4a7d954
Anthony Scopatz <scopatz@gmail.com>
2021-04-21 18:19:11 -0500

    update build and deps

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,19 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -ex
 
-export LDFLAGS="-L${PREFIX}/lib $LDFLAGS"
-export CPPFLAGS="-I${PREFIX}/include $CPPFLAGS"
-export CFLAGS="-fPIC $CFLAGS"
-
-./configure --prefix=$PREFIX
-
-make -j$CPU_COUNT
-make check -j$CPU_COUNT
-make install -j$CPU_COUNT
-
-
-# Conda uses only /lib for everything.
-mkdir -p $PREFIX/lib
-mv $PREFIX/lib64/* $PREFIX/lib
-
-cd $PREFIX
-find . -type f -name "*.la" -exec rm -rf '{}' \; -print
+./waf configure --prefix=$PREFIX
+./waf
+./waf install
